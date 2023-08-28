f7ccedf17a617f845dd9a377ba0897a3a9c4a219
Félix <Felix.sotelo.q@outlook.es>
2023-08-28 10:25:07 -0400

    Initial Commit

---
+++ AppetizersApp/AppetizersAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersAppApp.swift
+//  AppetizersApp
+//
+//  Created by Felix Sotelo on 28-08-23.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
